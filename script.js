f7e337108529a819a9306ef07e69c7e3c99123e8
Nghia Le <davidle314@hotmail.com>
2022-04-07 04:21:53 -0700

    refactor the code to make it save to local

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
+//declare var for save button
+var saveBtn = $(".save-btn")
 // function savebtn to save the text in the middle column
-$( document ).ready(function() {
-    $( ".save-btn col" ).click( function() {
-        var input = $( this ).siblings( ".col-lg-6").val();
-        var time = $( this ).siblings(".row").text();
+  saveBtn.on("click", function () {
 
+      var time = $(this).siblings("#hour").text();
+    var input = $(this).siblings(".Note").val();
+    
 
-        localStorage.setItem(time, input);
-        console.log(ready)
-    }) 
-}) 
-// function with time track  past,present,future
-function timeUpdate(){
+    localStorage.setItem(time, input);
+  });
+  // function with time track  past,present,future
+  function timeUpdate() {
     var currentTime = moment().hour();
 
-    $(".row").each(function() {
-        var currtime = parseInt($(this).attr(".col"));
+    $(".time-block").each(function () {
+      var currtime = parseInt($(this).attr("id"));
 
-        if (currtime > currentTime) {
-            $(this).addClass("future");
-         } else if (currtime < currentTime) {
-             $(this).removeClass("future");
-             $(this).addClass("past");
-         } else {
-             $(this).addClass("present")
-         }
-    })
+      if (currtime > currentTime) {
+        $(this).addClass("future");
+      } else if (currtime < currentTime) {
+        $(this).removeClass("future");
+        $(this).addClass("past");
+      } else {
+        $(this).addClass("present");
+      }
+    });
+  }
 
-
-}
